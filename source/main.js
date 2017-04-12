8b9ef6cee0234e32703d5254b94aeb8ec2beb48e
Alexander Davidson <alexander.harlan.davidson@gmail.com>
2017-04-12 13:01:52 -0700

    blocked out basic framework for ship

---
+++ source/main.js
@@ -0,0 +1,26 @@
+$('.ui.accordion')
+  .accordion()
+;
+
+$('.ui.dropdown')
+  .dropdown()
+;
+
+$('.ui.modal')
+  .modal()
+;
+
+$('.ui.embed').embed();
+
+// $('.ui.sticky')
+//   .sticky({
+//     context: '#ship-m50',
+//     pushing: true
+//   })
+// ;
+//
+// $(document).ready(function(){
+//      $('#modal-350r-button').click(function(){
+//         $('#modal-350r').modal('show');
+//      });
+// });
